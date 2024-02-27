96566201e830a9eda4ebfaa8e170d0022e8f0ec9
GitEsense <klg-hunter@mail.ru>
2024-02-27 10:12:00 +0200

    5.19. Домашнее задание - Классы - v.2
    Внесены некоторые исправления, была ошибка логики получения и записи

--- exercises/5-map/index.ts
+++ exercises/5-map/index.ts
@@ -12,12 +12,8 @@ interface IBucket {
     data: BucketElement[];
 }
 
-interface IResultMessage {
-    success: boolean;
-    message: string;
-}
 class ManualMap {
-    private buckets: IBucket[] = [];
+    private _buckets: IBucket[] = [];
 
     private stringToHash(key: string) {
         return `${key.length}`.padStart(3, '00').padEnd(6, '001');
@@ -25,17 +21,17 @@ class ManualMap {
 
     private findBucket(key: string) {
         const hash = this.stringToHash(key);
-        const bucket = this.buckets.find((f) => f.hash === hash);
+        const bucket = this._buckets.find((f) => f.hash === hash);
         return bucket;
     }
 
-    set(key: string, value: string | number): IResultMessage {
+    set(key: string, value: string | number): void {
         const hash = this.stringToHash(key);
-        const item = this.buckets.find((f) => f.hash === hash);
+        const item = this._buckets.find((f) => f.hash === hash);
         const bucket: BucketElement = [key, value];
         let message: string;
         if (!item) {
-            this.buckets.push({ hash, data: [bucket] });
+            this._buckets.push({ hash, data: [bucket] });
             message = `Новое значение успешно добавлено и доступно с ключом "${key}"`;
         } else {
             const index = item.data.findIndex(([f]) => f === key);
@@ -44,48 +40,55 @@ class ManualMap {
                 item.data[index] = bucket;
             } else {
                 message = `Значение с ключом "${key}" дописано в bucket`;
+                item.data = [...item.data, bucket];
             }
-            item.data = [...item.data, bucket];
         }
-        return { success: true, message };
+        this.log({ success: true, message });
     }
 
     get(key: string): BucketElement[] | undefined {
         const bucket = this.findBucket(key);
-        return bucket?.data.filter((item) => item[0] !== key);
+        return bucket?.data.filter((item) => item[0] === key);
     }
-    delete(key: string): IResultMessage {
+    delete(key: string): void {
         const bucket = this.findBucket(key);
-        console.log(bucket);
         if (!bucket) {
-            return { success: false, message: 'Ключ не найден' };
+            this.log({ success: false, message: 'Ключ не найден' });
         } else {
             bucket.data = bucket?.data.filter((item) => item[0] !== key);
         }
-        return { success: true, message: `Значение с ключом "${key}" успешно удалено` };
+        this.log({ success: true, message: `Значение с ключом "${key}" успешно удалено` });
     }
     clear(): void {
-        this.buckets = [];
+        this._buckets = [];
     }
-    get log(): IBucket[] {
-        return this.buckets;
+    private log(message: string | Record<string, unknown>): void {
+        console.log(message);
+    }
+
+    get buckets() {
+        return this._buckets;
     }
 }
 
 const manualMap = new ManualMap();
-console.log(manualMap.set('London', 22));
-console.log(manualMap.set('London', 21));
-console.log(manualMap.set('London', 22));
-console.log(manualMap.set('Berlin', 22));
-console.log(manualMap.set('San-Diego', 22));
-console.log(manualMap.get('London'));
-
-console.log(manualMap.set('London', 22));
+manualMap.set('London', 22);
+manualMap.set('London', 21);
 manualMap.set('Berlin', 22);
 
-console.log(manualMap.log);
-console.log(manualMap.delete('Berlin'));
-console.log(manualMap.log);
+console.log(manualMap.get('Berlin'));
+manualMap.delete('Berlin');
 
-console.log(manualMap.delete('Berlin'));
-console.log(manualMap.log);
+manualMap.set('San-Diego', 22);
+
+console.log(manualMap.get('Berlin'));
+
+console.log(manualMap.get('London'));
+manualMap.delete('London');
+
+console.log(manualMap.get('London'));
+manualMap.delete('Berlin');
+
+console.log(manualMap.buckets);
+manualMap.clear();
+console.log(manualMap.buckets);
