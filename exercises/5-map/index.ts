9d5c7629fc9f7a0fd0ab06169fd57656e40d27a6
GitEsense <klg-hunter@mail.ru>
2024-03-03 17:29:05 +0200

    5.19. Домашнее задание - Классы - v.3
    Обновлена и добавлена hash функция, добавлена генерация данных для тестирования

--- exercises/5-map/index.ts
+++ exercises/5-map/index.ts
@@ -8,87 +8,112 @@
 
 type BucketElement = [string, string | number];
 interface IBucket {
-    hash: string;
-    data: BucketElement[];
+  hash: number;
+  data: BucketElement[];
 }
 
 class ManualMap {
-    private _buckets: IBucket[] = [];
+  private _buckets: IBucket[] = [];
 
-    private stringToHash(key: string) {
-        return `${key.length}`.padStart(3, '00').padEnd(6, '001');
+  private stringToHash(key: string) {
+    let hashVal = 0;
+    for (let i = 0; i < key.length; i++) {
+      const charCode = key.charCodeAt(i);
+      hashVal = (hashVal * 27 + charCode) % key.length;
     }
+    return hashVal;
+  }
 
-    private findBucket(key: string) {
-        const hash = this.stringToHash(key);
-        const bucket = this._buckets.find((f) => f.hash === hash);
-        return bucket;
-    }
+  private findBucket(key: string) {
+    const hash = this.stringToHash(key);
+    const bucket = this._buckets.find((f) => f.hash === hash);
+    return bucket;
+  }
 
-    set(key: string, value: string | number): void {
-        const hash = this.stringToHash(key);
-        const item = this._buckets.find((f) => f.hash === hash);
-        const bucket: BucketElement = [key, value];
-        let message: string;
-        if (!item) {
-            this._buckets.push({ hash, data: [bucket] });
-            message = `Новое значение успешно добавлено и доступно с ключом "${key}"`;
-        } else {
-            const index = item.data.findIndex(([f]) => f === key);
-            if (index >= 0) {
-                message = `Значение с ключом "${key}" перезаписано`;
-                item.data[index] = bucket;
-            } else {
-                message = `Значение с ключом "${key}" дописано в bucket`;
-                item.data = [...item.data, bucket];
-            }
-        }
-        this.log({ success: true, message });
+  set(key: string, value: string | number): void {
+    const hash = this.stringToHash(key);
+    const item = this._buckets.find((f) => f.hash === hash);
+    const bucket: BucketElement = [key, value];
+    let message: string;
+    if (!item) {
+      this._buckets.push({ hash, data: [bucket] });
+      message = `Новое значение успешно добавлено и доступно с ключом "${key}"`;
+    } else {
+      const index = item.data.findIndex(([f]) => f === key);
+      if (index >= 0) {
+        message = `Значение с ключом "${key}" перезаписано`;
+        item.data[index] = bucket;
+      } else {
+        message = `Значение с ключом "${key}" дописано в bucket`;
+        item.data = [...item.data, bucket];
+      }
     }
+    this.log({ success: true, message });
+  }
 
-    get(key: string): BucketElement[] | undefined {
-        const bucket = this.findBucket(key);
-        return bucket?.data.filter((item) => item[0] === key);
+  get(key: string): BucketElement[] | undefined {
+    const bucket = this.findBucket(key);
+    return bucket?.data.filter((item) => item[0] === key);
+  }
+  delete(key: string): void {
+    const bucket = this.findBucket(key);
+    if (!bucket) {
+      this.log({ success: false, message: "Ключ не найден" });
+    } else {
+      bucket.data = bucket?.data.filter((item) => item[0] !== key);
     }
-    delete(key: string): void {
-        const bucket = this.findBucket(key);
-        if (!bucket) {
-            this.log({ success: false, message: 'Ключ не найден' });
-        } else {
-            bucket.data = bucket?.data.filter((item) => item[0] !== key);
-        }
-        this.log({ success: true, message: `Значение с ключом "${key}" успешно удалено` });
-    }
-    clear(): void {
-        this._buckets = [];
-    }
-    private log(message: string | Record<string, unknown>): void {
-        console.log(message);
-    }
+    this.log({
+      success: true,
+      message: `Значение с ключом "${key}" успешно удалено`,
+    });
+  }
+  clear(): void {
+    this._buckets = [];
+  }
+  private log(message: string | Record<string, unknown>): void {
+    console.log(message);
+  }
 
-    get buckets() {
-        return this._buckets;
-    }
+  get buckets() {
+    return this._buckets;
+  }
 }
 
 const manualMap = new ManualMap();
-manualMap.set('London', 22);
-manualMap.set('London', 21);
-manualMap.set('Berlin', 22);
+const cityArray = [
+  "Iberia",
+  "Kraemer",
+  "Libertytown",
+  "Deseret",
+  "Catharine",
+  "Mammoth",
+  "Allendale",
+  "Tetherow",
+  "Sussex",
+  "Valle",
+  "Shepardsville",
+  "Manchester",
+  "Englevale",
+  "Belmont",
+  "Brogan",
+  "Hobucken",
+  "Gilmore",
+  "Sehili",
+  "Woodlands",
+];
 
-console.log(manualMap.get('Berlin'));
-manualMap.delete('Berlin');
-
-manualMap.set('San-Diego', 22);
-
-console.log(manualMap.get('Berlin'));
-
-console.log(manualMap.get('London'));
-manualMap.delete('London');
-
-console.log(manualMap.get('London'));
-manualMap.delete('Berlin');
+cityArray.map((x) => manualMap.set(x, Math.round(Math.random() * 1500)));
 
 console.log(manualMap.buckets);
-manualMap.clear();
+const randomIndexStart = Math.floor(Math.random() * cityArray.length);
+const randomIndexEnd =
+  Math.floor(Math.random() * cityArray.length) + randomIndexStart;
+
+cityArray
+  .slice(randomIndexStart, randomIndexEnd)
+  .map((x) =>
+    Math.random() > 0.5
+      ? manualMap.delete(x)
+      : manualMap.set(x, Math.round(Math.random() * 1500))
+  );
 console.log(manualMap.buckets);
